75113651ed233bb5a07d081fa203e3513eb80778
d.kovalenko <d.kovalenko@postgrespro.ru>
2025-04-10 16:41:10 +0300

    setup.py is updated

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-import sys
+#import sys
 
 try:
     from setuptools import setup
@@ -16,15 +16,28 @@ with open('README.md', 'r') as f:
 setup(
     version='1.0.0',
     name='testgres.postgres_configuration',
-    packages=['testgres.postgres_configuration'],
+    packages=[
+        'testgres.postgres_configuration',
+        'testgres.postgres_configuration.core',
+        'testgres.postgres_configuration.core.option',
+        'testgres.postgres_configuration.core.option.handlers',
+        'testgres.postgres_configuration.core.option.handlers.add',
+        'testgres.postgres_configuration.core.option.handlers.get_value',
+        'testgres.postgres_configuration.core.option.handlers.prepare_get_value',
+        'testgres.postgres_configuration.core.option.handlers.prepare_set_value',
+        'testgres.postgres_configuration.core.option.handlers.prepare_set_value_item',
+        'testgres.postgres_configuration.core.option.handlers.set_value',
+        'testgres.postgres_configuration.core.option.handlers.set_value_item',
+        'testgres.postgres_configuration.core.option.handlers.write',
+    ],
     package_dir={'testgres.postgres_configuration': 'src'},
     description='PostgreSQL Configuration Python Library',
-    #url='https://github.com/postgrespro/testgres',
+    url='https://git.postgrespro.ru/d.kovalenko/testgres.postgres_configuration',
     long_description=readme,
     long_description_content_type='text/markdown',
     license='PostgreSQL',
     author='Postgres Professional',
-    author_email='testgres@postgrespro.ru',
+    author_email='d.kovalenko@postgrespro.ru',
     keywords=['postgresql', 'postgres', 'test'],
     install_requires=install_requires,
     classifiers=[],
