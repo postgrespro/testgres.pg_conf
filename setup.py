95f16ac771ff5460b7346f3da48b9543ea515af9
d.kovalenko <d.kovalenko@postgrespro.ru>
2025-04-27 23:10:50 +0300

    setup.py is updated [black]

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # //////////////////////////////////////////////////////////////////////////////
 # Postgres Pro. PostgreSQL Configuration Python Library.
 
-#import sys
+# import sys
 
 try:
     from setuptools import setup
@@ -9,43 +9,42 @@ except ImportError:
     from distutils.core import setup
 
 # Basic dependencies
-install_requires = [
-]
+install_requires = []
 
 # Get contents of README file
-with open('README.md', 'r') as f:
+with open("README.md", "r") as f:
     readme = f.read()
 
 setup(
-    version='0.0.1',
-    name='testgres.postgres_configuration',
+    version="0.0.1",
+    name="testgres.postgres_configuration",
     packages=[
-        'testgres.postgres_configuration',
-        'testgres.postgres_configuration.abstract',
-        'testgres.postgres_configuration.abstract.v00',
-        'testgres.postgres_configuration.implementation',
-        'testgres.postgres_configuration.implementation.v00',
-        'testgres.postgres_configuration.core',
-        'testgres.postgres_configuration.core.option',
-        'testgres.postgres_configuration.core.option.handlers',
-        'testgres.postgres_configuration.core.option.handlers.add',
-        'testgres.postgres_configuration.core.option.handlers.get_value',
-        'testgres.postgres_configuration.core.option.handlers.prepare_get_value',
-        'testgres.postgres_configuration.core.option.handlers.prepare_set_value',
-        'testgres.postgres_configuration.core.option.handlers.prepare_set_value_item',
-        'testgres.postgres_configuration.core.option.handlers.set_value',
-        'testgres.postgres_configuration.core.option.handlers.set_value_item',
-        'testgres.postgres_configuration.core.option.handlers.write',
+        "testgres.postgres_configuration",
+        "testgres.postgres_configuration.abstract",
+        "testgres.postgres_configuration.abstract.v00",
+        "testgres.postgres_configuration.implementation",
+        "testgres.postgres_configuration.implementation.v00",
+        "testgres.postgres_configuration.core",
+        "testgres.postgres_configuration.core.option",
+        "testgres.postgres_configuration.core.option.handlers",
+        "testgres.postgres_configuration.core.option.handlers.add",
+        "testgres.postgres_configuration.core.option.handlers.get_value",
+        "testgres.postgres_configuration.core.option.handlers.prepare_get_value",
+        "testgres.postgres_configuration.core.option.handlers.prepare_set_value",
+        "testgres.postgres_configuration.core.option.handlers.prepare_set_value_item",
+        "testgres.postgres_configuration.core.option.handlers.set_value",
+        "testgres.postgres_configuration.core.option.handlers.set_value_item",
+        "testgres.postgres_configuration.core.option.handlers.write",
     ],
-    package_dir={'testgres.postgres_configuration': 'src'},
-    description='PostgreSQL Configuration Python Library',
-    url='https://git.postgrespro.ru/d.kovalenko/testgres.postgres_configuration',
+    package_dir={"testgres.postgres_configuration": "src"},
+    description="PostgreSQL Configuration Python Library",
+    url="https://git.postgrespro.ru/d.kovalenko/testgres.postgres_configuration",
     long_description=readme,
-    long_description_content_type='text/markdown',
-    license='PostgreSQL',
-    author='Postgres Professional',
-    author_email='d.kovalenko@postgrespro.ru',
-    keywords=['postgresql', 'postgres', 'test'],
+    long_description_content_type="text/markdown",
+    license="PostgreSQL",
+    author="Postgres Professional",
+    author_email="d.kovalenko@postgrespro.ru",
+    keywords=["postgresql", "postgres", "test"],
     install_requires=install_requires,
     classifiers=[],
 )
