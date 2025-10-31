c8a4a04814f05d59d73b81335bc825d2f3c645ed
d.kovalenko <d.kovalenko@postgrespro.ru>
2025-10-31 22:06:32 +0300

    setup.py is corrected [url]

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
     ],
     package_dir={"testgres.postgres_configuration": "src"},
     description="PostgreSQL Configuration Python Library",
-    url="https://git.postgrespro.ru/d.kovalenko/testgres.postgres_configuration",
+    url="https://github.com/postgrespro/testgres.pg_conf",
     long_description=readme,
     long_description_content_type="text/markdown",
     license="PostgreSQL",
