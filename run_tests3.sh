85ca05e43da01210693e385e7a2ef00040b37f16
Dmitry Kovalenko <dmitry.lipetsk@gmail.com>
2026-06-18 09:22:01 +0300

    CI: Python 3.7.3 (4, 5) is added (#32)
    * CI: Python 3.7.3 (4, 5) is added.
    
    Testgres supports py3.7.3+.
    
    * CI: astralinux_1_7 is added

--- run_tests3.sh
+++ run_tests3.sh
@@ -7,7 +7,7 @@ set -eux
 # prepare python environment
 VENV_PATH="/tmp/testgres_venv"
 rm -rf $VENV_PATH
-python -m venv "${VENV_PATH}"
+${PYTHON_BINARY} -m venv "${VENV_PATH}"
 export VIRTUAL_ENV_DISABLE_PROMPT=1
 source "${VENV_PATH}/bin/activate"
 
